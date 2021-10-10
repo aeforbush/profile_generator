0010a4f2fad81b277f25259a8964aa6710fb04ee
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-10 15:16:14 -0600

    Employee test passed

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,4 +1,60 @@
-const { test } = require('@jest/globals');
-const Employee = require('../lib/Employee');
+const Employee = require("../lib/Employee");
 
-test( )
+describe("Employee", () => {
+  it("Can create employee", () => {
+    const e = new Employee();
+    expect(typeof e).toBe("object");
+  });
+
+  it("Can set name via constructor arguments", () => {
+    const name = "Ida";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+  });
+
+  it("Can set id via constructor argument", () => {
+    const testValue = 50;
+    const e = new Employee("Ida", testValue);
+    expect(e.id).toBe(testValue);
+  });
+
+  it("Can set email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+  });
+
+  describe("getName", () => {
+    it("Can get name via getName()", () => {
+      const testValue = "Ida";
+      const e = new Employee(testValue);
+      expect(e.getName()).toBe(testValue);
+    });
+  });
+  describe("getId", () => {
+    it("Can get id via getId()", () => {
+      const testValue = 50;
+      const e = new Employee("Ida", testValue);
+      expect(e.getId()).toBe(testValue);
+    });
+  });
+
+  describe("getEmail", () => {
+    if (
+      ("Can get email via getEmail()",
+      () => {
+        const testValue = "test@test.com";
+        const e = new Employee("Ida", 1, testValue);
+        expect(e.getEmail()).toBe(testValue);
+      })
+    );
+  });
+
+  describe("getRole", () => {
+    it("Can get role via getRole()", () => {
+      const testValue = "Employee";
+      const e = new Employee("Ida", 1, "test@test.com");
+      expect(e.getRole()).toBe(testValue);
+    });
+  });
+});
